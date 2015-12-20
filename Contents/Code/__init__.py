31a96407f331d1ae9b32a43ee12e8e9048001bac
Twoure <twoure@aol.com>
2015-12-20 03:23:18 -0600

    First push of local code

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,100 @@
+####################################################################################################
+#                                                                                                  #
+#                                     GreekTV Plex Channel                                         #
+#                                                                                                  #
+####################################################################################################
+#from updater import Updater  #uncomment when ready to add updater
+
+# set global variables
+TITLE = L('title')
+PREFIX = '/video/greektv'
+XML_URL = 'http://greektv.upg.gr/xml/gr_streams_avail.xml'
+
+# set background art and icon defaults
+ICON = 'icon-default.png'
+
+####################################################################################################
+def Start():
+    HTTP.CacheTime = 0
+
+    ObjectContainer.title1 = TITLE
+    #ObjectContainer.art = R(ART)
+
+    DirectoryObject.thumb = R(ICON)
+    #DirectoryObject.art = R(ART)
+
+    #InputDirectoryObject.art = R(ART)
+
+    #VideoClipObject.art = R(ART)
+
+    #ValidatePrefs()
+
+####################################################################################################
+@handler(PREFIX, TITLE, thumb=ICON)
+def MainMenu():
+    oc = ObjectContainer(title2=TITLE, no_cache=True)
+
+    #Updater(PREFIX + '/updater', oc)  #uncomment when ready to add updater
+
+    xml = XML.ElementFromURL(XML_URL)
+
+    for node in xml[0][0]:
+        title = node.get('title')
+        thumb = node.get('hdposterurl')
+        genrel = node.get('genrel')
+        url = node.get('url')
+        ishd = node.get('ishd')
+        bitrate = node.get('bitrate')
+        streamformat = node.get('streamformat')
+        live = node.get('live')
+
+        if live == 'true':
+            oc.add(
+                CreateVideoClipObject(
+                    title=title, thumb=thumb, url=url
+                    )
+                )
+
+    return oc
+
+####################################################################################################
+@route(PREFIX + '/createvideoclipobject', include_container=bool)
+def CreateVideoClipObject(title, thumb, url, include_container=False, *args, **kwargs):
+
+    video_object = VideoClipObject(
+        key=Callback(CreateVideoClipObject, title=title, thumb=thumb, url=url, include_container=True),
+        rating_key=url,
+        title=title,
+        thumb=thumb,
+        content_rating='NR',
+        url=url,
+        items=GetMediaObject(url)
+        )
+
+    if include_container:
+        return ObjectContainer(objects=[video_object])
+    else:
+        return video_object
+
+####################################################################################################
+def GetMediaObject(url):
+
+    mo = [
+        MediaObject(
+            parts=[
+                PartObject(
+                    key=HTTPLiveStreamURL(Callback(PlayVideo, url=url))
+                    )
+                ],
+
+            )
+        ]
+
+    return mo
+
+####################################################################################################
+@indirect
+@route(PREFIX + '/playvideo.m3u8')
+def PlayVideo(url):
+
+    return IndirectResponse(VideoClipObject, key=url)
---
+++ README.md
@@ -0,0 +1,39 @@
+GreekTV.bundle
+==============
+
+This is a plugin that creates a new channel in [Plex Media Server](https://plex.tv) to view content from [GreekTV](http://greektv.upg.gr/). It is currently under development and as such, should be considered alpha software and potentially unstable.
+
+> **Note:** the author of this plugin has no affiliation with [LihatTV](http://lihattv.com/) nor the owners of the content that they host.
+
+## Features
+
+- Watch Live Greek IPTV by UPG.GR
+
+## Channel Support
+
+##### Plex Media Server:
+- Tested Working:
+  - Ubuntu 14.04 LTS: PMS version 0.9.14.5
+
+##### Plex Clients:
+- Tested Working:
+  - Android (KitKat 4.4.2)
+  - Plex/Web (2.4.39) (Disable/Enable Direct play depending on stream)
+
+## How To Install
+
+- [Download](https://github.com/Twoure/GreakTV.bundle/archive/master.zip) and install it by following the Plex [instructions](https://support.plex.tv/hc/en-us/articles/201187656-How-do-I-manually-install-a-channel-) or the instructions below.
+  - Unzip and rename the folder to "GreekTV.bundle"
+  - Copy GreekTV.bundle into the PMS [Plug-ins](https://support.plex.tv/hc/en-us/articles/201106098-How-do-I-find-the-Plug-Ins-folder-) directory
+  - ~~Restart PMS~~ **This is old, should not have to restart PMS.  If channel does not appear then Restart PMS**
+
+## Issues
+
+- Plex Web Client: Must disable Direct Play to make Crossdomain m3u8 streams work, but this breaks the other streams that don't depend on Crossdomains.
+- Chromecast not working yet
+- _Alpha TV_ has a 404 error.  Acess is denied to site.  Maybe it's a regional deal
+- Some streams take some time to load
+
+## Changelog
+
+**1.0.0** - 12/20/15 - First push of local code
