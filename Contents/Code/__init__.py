c9f0f76ac9430ba34368501e7ffe0bb1ab1066c3
Twoure <twoure@aol.com>
2016-11-21 19:04:25 -0600

    New XML, with categories

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -7,7 +7,7 @@
 # set global variables
 TITLE = 'GreekTV'
 PREFIX = '/video/greektv'
-XML_URL = 'http://greektv.upg.gr/api/?type=plex'
+XML_URL_ROKU = 'http://greektv.upg.gr/api/?type=rokuxml'
 
 # set background art and icon defaults
 ICON = 'icon-default.jpg'
@@ -15,7 +15,7 @@ ART = 'art-default.jpg'
 
 ####################################################################################################
 def Start():
-    HTTP.CacheTime = 0
+    HTTP.CacheTime = 10
 
     ObjectContainer.title1 = TITLE
     ObjectContainer.art = R(ART)
@@ -28,50 +28,114 @@ def Start():
 ####################################################################################################
 @handler(PREFIX, TITLE, thumb=ICON, art=ART)
 def MainMenu():
-    oc = ObjectContainer(title2=TITLE, no_cache=True)
+    oc = ObjectContainer(title2=TITLE)
 
-    xml = XML.ElementFromURL(XML_URL)
-
-    for node in xml[0][0]:
-        title = node.get('title')
-        thumb = node.get('hdposterurl')
-        #genrel = node.get('genrel')
-        url = node.get('url')
-        #ishd = node.get('ishd')
-        active = node.get('active')
-
-        if (active == 'true') or (active == '1'):
-            oc.add(
-                CreateVideoClipObject(
-                    title=title, thumb=thumb, url=url
-                    )
-                )
+    oc.add(DirectoryObject(
+        key=Callback(DirectoryList, category='all'),
+        title='All',
+        summary='List all available Channels',
+        ))
+    oc.add(DirectoryObject(
+        key=Callback(CategoryList),
+        title='Categories',
+        summary='List Channels by Categories',
+        ))
 
     return oc
 
 ####################################################################################################
-@route(PREFIX + '/createvideoclipobject', include_container=bool)
-def CreateVideoClipObject(title, thumb, url, include_container=False, *args, **kwargs):
+@route(PREFIX + '/categories')
+def CategoryList():
+    oc = ObjectContainer(title2='Categories', no_cache=True)
 
-    video_object = VideoClipObject(
-        key=Callback(CreateVideoClipObject, title=title, thumb=thumb, url=url, include_container=True),
+    html = HTML.ElementFromURL(XML_URL_ROKU)
+    cat_list = list()
+    for c in html.xpath('//category'):
+        cat_list.append({
+            'title': c.get('title'), 'thumb': c.get('hd_img'),
+            'summary': c.get('description')
+            })
+
+    for c in Util.ListSortedByKey(cat_list, 'title'):
+        oc.add(DirectoryObject(
+            key=Callback(DirectoryList, category=c['title']),
+            title=c['title'],
+            thumb=Resource.ContentsOfURLWithFallback(c['thumb'], ICON),
+            summary=c['summary'],
+            ))
+
+    return oc
+
+####################################################################################################
+@route(PREFIX + '/directory-list')
+def DirectoryList(category):
+    oc = ObjectContainer(title2=category.title(), no_cache=True)
+
+    html = HTML.ElementFromURL(XML_URL_ROKU)
+
+    # setup feed node depending of category/all list
+    feed_node = '//feed[@title' + ('' if category == 'all' else '="{}"'.format(category)) + ']/item'
+
+    c_list = list()
+    for item in html.xpath(feed_node):
+        t = dict()
+        # setup title & thumb
+        t['title'] = item.xpath('./title/text()')[0]
+        t['thumb'] = item.get('hdimg')
+
+        # setup summary
+        s = item.xpath('./description/text()')
+        t['summary'] = s[0] if s else ""
+
+        # check stream format, only allow m3u8 streams
+        fmt = item.xpath('./media/streamformat/text()')
+        fmt = fmt[0] if fmt else None
+        if fmt.lower() != 'hls':
+            # skip stream if not hls
+            continue
+
+        # setup stream URL
+        t['url'] = item.xpath('./media/streamurl/text()')[0]
+        c_list.append(t)
+
+    for s in Util.ListSortedByKey(c_list, 'title'):
+        oc.add(CreateVCO(
+            title=s['title'], thumb=s['thumb'],
+            summary=s['summary'], url=s['url'],
+            ))
+
+    return oc
+
+####################################################################################################
+@route(PREFIX + '/create-video-clipobject', include_container=bool)
+def CreateVCO(title, thumb, url, summary=None, include_container=False, *args, **kwargs):
+
+    vo = VideoClipObject(
+        key=Callback(CreateVCO,
+            title=title, thumb=thumb, url=url,
+            summary=summary, include_container=True
+            ),
         rating_key=url,
         title=title,
         thumb=thumb,
+        summary=summary,
+        year=int(Datetime.Now().year),  # set to current year
+        source_title=TITLE,  # set to channel name
         content_rating='NR',
         items=GetMediaObject(url)
         )
 
     if include_container:
-        return ObjectContainer(objects=[video_object])
-    else:
-        return video_object
+        return ObjectContainer(objects=[vo])
+    return vo
 
 ####################################################################################################
 def GetMediaObject(url):
 
-    mo = [
+    return [
         MediaObject(
+            audio_channels=2,
+            optimized_for_streaming=True,
             parts=[
                 PartObject(
                     key=HTTPLiveStreamURL(Callback(PlayVideo, url=url))
@@ -79,8 +143,6 @@ def GetMediaObject(url):
                 ]
             )
         ]
-
-    return mo
 
 ####################################################################################################
 @indirect
--- README.md
+++ README.md
@@ -14,29 +14,20 @@ The channels are fed from an xml file that is updated every 10 minutes. This mak
 
 ##### Plex Media Server:
 - Tested Working:
-  - Ubuntu 14.04 LTS: PMS version 0.9.15
+  - Ubuntu 14.04 LTS: PMS version 1.2.7
 
 ##### Plex Clients:
 - Tested Working:
   - Android (KitKat 4.4.2)
-  - Plex/Web (2.4.39) (Disable/Enable Direct play depending on stream)
+  - Plex/Web (2.10.11) _(Disable/Enable Direct play/stream depending on stream)_
 
 ## How To Install
 
-- [Download](https://github.com/Twoure/GreekTV.bundle/releases) latest release and install it by following the Plex [instructions](https://support.plex.tv/hc/en-us/articles/201187656-How-do-I-manually-install-a-channel-) or the instructions below.
-  - Unzip and rename the folder to "GreekTV.bundle"
-  - Copy GreekTV.bundle into the PMS [Plug-ins](https://support.plex.tv/hc/en-us/articles/201106098-How-do-I-find-the-Plug-Ins-folder-) directory
-  - ~~Restart PMS~~ **This is old, should not have to restart PMS.  If channel does not appear then Restart PMS**
+Follow the [Adding, Removing, and Updating a Channel](https://support.plex.tv/hc/en-us/articles/201053758-Adding-Removing-and-Updating-a-Channel) instructions.
 
 ## Issues
 
-- Plex Web Client: Must disable Direct Play to make Crossdomain m3u8 streams work, but this breaks the other streams that don't depend on Crossdomains.
+- Plex Web Client: Must disable Direct Play/Stream to make Crossdomain `m3u8` streams work, but this breaks the other streams that don't depend on Crossdomains.
 - Chromecast not working yet
 - AlphaTV is blocked for the US and some other countries
 - Some streams take some time to load
-
-## Changelog
-
-**1.0.1** - 12/23/15 - New Icon and Art. Added updater.
-
-**1.0.0** - 12/20/15 - First push of local code
